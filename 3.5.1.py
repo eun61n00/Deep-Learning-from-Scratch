e1c32dfd9d2b5a8d982fc10b2b4d202366a81bc0
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-02 13:24:44 +0900

    Create 3.5.1.py

---
+++ 3.5.1.py
@@ -0,0 +1,17 @@
+a = np.array([0.3, 2.9, 4.0])
+
+exp_a = np.exp(a) #지수함수
+print(exp_a)
+
+sum_exp_a = np.sum(exp_a)
+print(sum_exp_a)
+
+y = exp_a / sum_exp_a
+print(y)
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    
+    return y
