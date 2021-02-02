ad5ce303b98bcebd6f52dd745efa2b50541e2418
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-02 16:59:55 +0900

    Update 3.6.py

--- Ch3. 신경망/3.6.py
+++ Ch3. 신경망/3.6.py
@@ -1,6 +1,16 @@
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
 import sys, os
 sys.path.append(os.pardir)    #부모 디렉터리의 파일을 가져올 수 있도록 설정
-from dataset.mnist import load_mnist
+from mnist import load_mnist
 
 (x_train, t_train), (x_test, t_test) = \
     load_mnist(flatten=True, normalize = False)
@@ -10,6 +20,8 @@ print(x_train.shape)    #(60000, 784)
 print(t_train.shape)    #(60000,)
 print(x_test.shape)    #(10000, 784)
 print(x_test.shape)    #(10000,)
+
+
 
 import numpy as np
 from PIL import Image
@@ -26,7 +38,7 @@ lable = t_train[0]
 print(lable)    #5
 
 print(img.shape)    #(784,)
-img = img.reshape(28, 28)    #원래 이미지의 모양으로 변형
+img = img.reshape(28, 28)
 print(img.shape)    #(28, 28)
 
 img_show(img)
@@ -35,33 +47,48 @@ def get_data():
     (x_train, t_train), (x_test, t_test) = \
         load_mnist(normalize = True, flatten = True, one_hot_label = False)
     return x_test, t_test
-  
+
+import pickle
 def init_network():
     with open("sample_weight.pkl", 'rb') as f:
         network = pickle.load(f)
-        
     return network
 
 def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
-    
+
     a1 = np.dot(x, W1) + b1
     z1 = sigmoid(a1)
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
     y = softmax(a3)
-    
+
     return y
-  
+
 x, t = get_data()
 network = init_network()
 
 accuracy_cnt = 0
-for i in range(len()):
+for i in range(len(x)):
     y = predict(network, x[i])
     p = np.argmax(y)    # 확률이 가장 높은 원소의 인덱스를 얻는다.
     if p == t[i]:
-        accuracy_cnt += 
+        accuracy_cnt += 1
 print("Accuracy: " + str(float(accuracy_cnt) / len(x)))
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuray_cnt += np.sum(p == t[i:i+batch_size])
+
+print ("Accuracy: " + str(float(accuracy_cnt)/len(x)))
+
