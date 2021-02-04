889302f1df203c290b6f14e2d0cfbc93eea44425
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-04 21:18:07 +0900

    Update 4.2.3.py

--- Ch4. 신경망 학습/4.2.3.py
+++ Ch4. 신경망 학습/4.2.3.py
@@ -6,6 +6,12 @@ from dataset.mnist import load_mnist
 (x_train, t_train), (x_test, t_test) = \
     load_mnist(normalize = True, one_hot_label = True)
 
-print(x_train.shape)
-print(t_train.shape)
+print(x_train.shape) #(60000, 784)
+print(t_train.shape) ##(60000, 10) -> one_hot_lable = True라서 column이 10이 됨
 
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+print (batch_mask)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
