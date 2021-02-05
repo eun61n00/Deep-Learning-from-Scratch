f6c48efcd6a8f241bfa0ee99301980e7114fb3db
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-05 15:51:27 +0900

    Update 4.3.1.py

--- Ch4. 신경망 학습/4.3.1.py
+++ Ch4. 신경망 학습/4.3.1.py
@@ -1,1 +1,9 @@
-
+# 수치 미분의 나쁜 구현
+def numerical_diff(f, x):
+    h = 10e-50
+    return (f(x+h)-f(x)) / h
+  
+#개선
+def numerical_diff(f, x):
+    h = 1e-4 #0.0001
+    return (f(x+h) - f(x-h)) / (2*h)
