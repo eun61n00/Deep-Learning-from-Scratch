cf347ebf3913aecd3810f3151a8db4c56c44f103
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-05 17:58:15 +0900

    Update 4.4.1.py

--- Ch4. 신경망 학습/4.4.1.py
+++ Ch4. 신경망 학습/4.4.1.py
@@ -1,1 +1,7 @@
-
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_X
+    
+    for i in range(step_num):
+        grad = numerical_grdient(f, x)
+        x -= lr * grad
+        return x
