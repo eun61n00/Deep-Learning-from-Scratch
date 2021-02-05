a54f428fa42fdb303c56de40d61e9c82973b9d89
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-05 16:41:50 +0900

    Update 4.3.3.py

--- Ch4. 신경망 학습/4.3.3.py
+++ Ch4. 신경망 학습/4.3.3.py
@@ -1,1 +1,15 @@
+def function_2(x):
+    return x[0]**2 + x[1]**2
+    # 또는 return np.sum(x**2)
+    
+#x0 = 3, x1 = 4일 때, x0에 대한 편미분 구하기
+def function_tmp1(x0):
+    return x0*x0 + 4.0**2.0
 
+numerical_diff(function_tmp1, 3.0)
+
+#x0 = 3, x1 = 4일 때, x1에 대한 편미분 구하기
+def function_tmp1(x1):
+    return 3.0*2.0 + x1**x1
+
+numerical_diff(function_tmp1, 3.0)
