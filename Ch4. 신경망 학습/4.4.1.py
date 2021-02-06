43e404938a5bdf364415c640217c6f6d05b62ebe
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-06 15:08:47 +0900

    Update 4.4.1.py

--- Ch4. 신경망 학습/4.4.1.py
+++ Ch4. 신경망 학습/4.4.1.py
@@ -5,3 +5,10 @@ def gradient_descent(f, init_x, lr=0.01, step_num=100):
         grad = numerical_grdient(f, x)
         x -= lr * grad
         return x
+    
+#문제: 경사법으로  f((x0), (x1)) = (x0)^2 + (x1)^2의 최솟값을 구하라.
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+init_x = np.array([-3.0, 4.0])
+gradient_descent(function_2, init_x = init_x, lr=0.1, step_num=100)
