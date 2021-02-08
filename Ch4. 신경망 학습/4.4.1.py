c63a8dc6d8758112ce0a7ce344d973c971444629
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-08 14:07:48 +0900

    Update 4.4.1.py

--- Ch4. 신경망 학습/4.4.1.py
+++ Ch4. 신경망 학습/4.4.1.py
@@ -12,3 +12,9 @@ def function_2(x):
 
 init_x = np.array([-3.0, 4.0])
 gradient_descent(function_2, init_x = init_x, lr=0.1, step_num=100)
+
+#학습률이 너무 큰 예: lr = 10.0
+gradient_descent(function_2, init_x=init_x, lr=10.0, step_num=100)
+
+#학습률이 너무 작은 예: lr = 1e-10
+gradient_descent(function_2, init_x=init_x, lr=1e-10, step_num=100)
