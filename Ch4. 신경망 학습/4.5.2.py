159d11a3c09e5bc76319cb9bc07883d11ada1ced
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-09 23:56:09 +0900

    Update 4.5.2.py

--- Ch4. 신경망 학습/4.5.2.py
+++ Ch4. 신경망 학습/4.5.2.py
@@ -1,1 +1,33 @@
+import numpy as np
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+network = TwoLayerNet(input_size = 784, hidden_size=50, output_size = 10)
 
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(normalize = True, one_hot_label = True)
+
+train_loss_list = []
+
+#hyper parameter
+iters_num = 1000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+network = TwoLayerNet(input_size = 784, hidden_size = 50, output_size = 10) #신경망 구현
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    #기울기 계산
+    grad = net.numerical_gradient(x_batch, t_batch)
+    #grad = net.gradient(x_batch, t_batch) #성능 개선
+    
+    #매개변수 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate*grad[key]
+    
+    #학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_lsit.append(loss)
