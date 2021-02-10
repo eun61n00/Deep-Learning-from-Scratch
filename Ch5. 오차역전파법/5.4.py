ab4c78a1c1c156cc44a39729c58a8d26ccbda49c
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-10 15:41:33 +0900

    Update 5.4.py

--- Ch5. 오차역전파법/5.4.py
+++ Ch5. 오차역전파법/5.4.py
@@ -33,3 +33,45 @@ price = mul_tax_layer.forward(apple_price, tax)
 
 print(price) #220
 
+#5.4.2 덧셈 계층
+
+class AddLayer:
+    def __init__(self):
+        pass
+    
+    def forward(self, x, y):
+        out = x+y
+        return out
+    
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+    
+
+#사과 2개와 오렌지 3개를사는 상황 구현
+    
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+#계층들
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+#순전파
+apple_price = mul_apple_layer.forward(apple, apple_num) #(1)
+orange_price = mul_orange_layer.forward(orange, orange_num) #(2)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price) #(3)
+price = mul_tax_layer.forward(all_price, tax) #(4)
+
+#역전파
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice) #(4)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price) #(3)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price) #(2)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price) #(1)
