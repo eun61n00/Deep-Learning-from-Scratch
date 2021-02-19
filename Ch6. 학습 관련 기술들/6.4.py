b6b5ad6fb1c01fe4a3747884b7826e9c7cee859d
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-19 15:16:13 +0900

    Update 6.4.py

--- Ch6. 학습 관련 기술들/6.4.py
+++ Ch6. 학습 관련 기술들/6.4.py
@@ -1,1 +1,74 @@
+#6.4.1. 오버피팅
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize = True)
+#오버피팅을 재현하기 위해 학습데이터 수를 줄임
+x_train = x_trian[:300]
+t_train = t_train[:300]
+
+network = MultiLayerNet(input_size = 784, hidden_size_list = [100, 100, 100, 100, 100, 100], output_size = 10)
+optimizer = SGD(lr = 0.01) #학습률이 0.01dls SGD로 매개변수 갱신
+max_epocks = 201
+train_size = x_train.shape[0]
+batch_size = 100
+
+train_loss_list[]
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+epoch_cnt = 0
+
+for i in range(1000000000):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[bathc_mask]
+    
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        
+        epoch_cnt += 1
+        if epoch_cnt >= max_epochs:
+            break
+            
+#6.4.2 가중치 감소 weight decay
+def loss(self, x, t):
+        """손실 함수를 구한다.
+        
+        Parameters
+        ----------
+        x : 입력 데이터
+        t : 정답 레이블 
+        
+        Returns
+        -------
+        손실 함수의 값
+        """
+        y = self.predict(x)
+
+        weight_decay = 0
+        for idx in range(1, self.hidden_layer_num + 2):
+            W = self.params['W' + str(idx)]
+            weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W ** 2)
+
+        return self.last_layer.forward(y, t) + weight_decay
+
+ #6.4.3 드롭아웃 Dropout
+class Dropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+        
+    def forward(self, x, train_flg = True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+        
+    def backward(self, dout):
+        return dout * self.mask
+
 
